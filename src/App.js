570e28c929e789760cbc7f900fce6ba00285cb10
josemariaato <josemariaato@gmail.com>
2025-06-06 19:24:34 +0200

    Restaurar Login como página principal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from "react";
-import "./index.css";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return (
-    <div className="bg-red-100 text-center text-2xl text-red-700 p-10 rounded-2xl">
-      Esto es una prueba de TailwindCSS funcionando
-    </div>
-  );
+  return <LoginPage />;
 }
 
 export default App;
